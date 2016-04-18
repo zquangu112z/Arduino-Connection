fbf7108ac1368bb060f57b6560b8937048d499f4
zquangu112z <zquangu112z@gmail.com>
2016-04-19 02:02:19 +0700

    feat: add lib for arduino (.ino)

---
+++ ArduinoConnection.ino
@@ -0,0 +1,46 @@
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  Serial.println(1000,DEC);
+  Serial.println(800,DEC);
+  delay(1000);
+  Serial.println(300,DEC);
+  Serial.println(460,DEC);
+  delay(1000);
+  Serial.println(50,DEC);
+  Serial.println(360,DEC);
+  delay(1000);
+  Serial.println(70,DEC);
+  Serial.println(100,DEC);
+  delay(1000);
+  Serial.println(200,DEC);
+  Serial.println(30,DEC);
+  delay(1000);
+  Serial.println(20,DEC);
+  Serial.println(100,DEC);
+  delay(1000);
+
+  Serial.println(200,DEC);
+  Serial.println(300,DEC);
+  delay(1000);
+  Serial.println(400,DEC);
+  Serial.println(700,DEC);
+  delay(1000);
+  Serial.println(90,DEC);
+  Serial.println(900,DEC);
+  delay(1000);
+  Serial.println(80,DEC);
+  Serial.println(800,DEC);
+  delay(1000);
+  Serial.println(70,DEC);
+  Serial.println(700,DEC);
+  delay(1000);
+  Serial.println(200,DEC);
+  Serial.println(500,DEC);
+  delay(1000);
+  
+}
+
+
